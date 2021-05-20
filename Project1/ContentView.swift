890555eddfbbba64b0772c2b697a1c954c3c2150
Frank Solleveld <fsolleveld@outlook.com>
2021-05-20 13:28:59 +0200

    Finished split app

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -13,6 +13,16 @@ struct ContentView: View {
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -26,8 +36,16 @@ struct ContentView: View {
                         }
                     }
                 }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
                 Section {
-                    Text("€\(checkAmount)")
+                    Text("€\(totalPerPerson, specifier: "%.2f")")
                 }
             }
             .navigationTitle("WeSplit💰")
